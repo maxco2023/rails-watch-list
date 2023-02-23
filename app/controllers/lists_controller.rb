bf0d53079ca3babe666cf28e9689bac6e66c5d84
maxco2023 <maxime.coquerelle@gmail.com>
2023-02-23 16:35:39 +0100

    done2

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,48 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    @list.update(list_params)
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    if @list.destroy
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
-  has_many :bookmarksg
+  has_many :bookmarks
   validates :title, uniqueness: true, presence: true
   validates :overview, presence: true
 end

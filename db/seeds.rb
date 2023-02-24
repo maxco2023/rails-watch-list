474879387cb0f7fc0ff1696f1ba4b1be1a2a5438
maxco2023 <maxime.coquerelle@gmail.com>
2023-02-24 17:00:37 +0100

    la c'est fini

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require "faker"
 
+
+Bookmark.destroy_all
 Movie.destroy_all
 
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
